1bbe7fbd14a10b66330dbcf7c0e25596d379e29a
albashzia <ahmedalbashzia@gmail.com>
2026-05-14 18:56:28 +0500

    Defining the constructors of Appointment class

--- src/Appointment.java
+++ src/Appointment.java
@@ -5,4 +5,15 @@ public class Appointment {
     private Patient patient;
     private Date date;
 
+    Appointment(){
+        this.doctor = null;
+        this.patient = null;
+        this.date = null;
+    }
+
+    Appointment(Doctor doctor, Patient patient, Date date){
+        this.doctor = doctor;
+        this.patient = patient;
+        this.date = date;
+    }
 }
