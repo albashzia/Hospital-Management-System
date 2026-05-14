c1109296e93b999eadbea27ba98e820e59fb7066
albashzia <ahmedalbashzia@gmail.com>
2026-05-14 18:34:54 +0500

    Defining the default and parameterized constructor of Doctor class

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,3 +1,14 @@
 public class Doctor extends Person{
-    private String Specialization;
+    private String specialization;
+
+    Doctor(){
+        super();
+        this.specialization = null;
+    }
+
+    Doctor(String id, String name, int age, String specialization){
+        super(id,name,age);
+        this.specialization = specialization ;
+    }
+
 }
