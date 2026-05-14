376c4c9dbf99198716d6b0ba61cb286e76a38038
albashzia <ahmedalbashzia@gmail.com>
2026-05-14 18:40:34 +0500

    Creating the display method for Patient class which accesses the display method of Person class using super keyword

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,5 @@
+import javax.swing.text.html.HTMLDocument;
+
 public class Patient extends Person{
     private String illness;
 
@@ -17,6 +19,12 @@ public class Patient extends Person{
 
     public String getIllness(){
         return this.illness;
+    }
+
+    public void display(){
+        System.out.println("===Patient Details===\n");
+        super.display();
+        System.out.println("Illness: "+ getIllness());
     }
 
 }
