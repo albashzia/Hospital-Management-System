14cb937ff1d0d30dd8ea9b593a0a9eb87f7161fd
albashzia <ahmedalbashzia@gmail.com>
2026-05-14 18:22:56 +0500

    Defining the constructors of Person class

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,17 @@ abstract public class Person {
     public String id;
     public String name;
     public int age;
+
+    Person(){
+        id = "0";
+        name = null;
+        age = 0;
+    }
+
+    Person(String id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
 }
