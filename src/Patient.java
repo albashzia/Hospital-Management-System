7918f53691e44578ce68e47a59f20a2587e44a5b
albashzia <ahmedalbashzia@gmail.com>
2026-05-14 18:32:19 +0500

    Defining the default and parameterized constructor of Patient class

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,14 @@
 public class Patient extends Person{
     private String illness;
+
+    Patient(){
+        super();
+        this.illness = null;
+    }
+
+    Patient(String id, String name, int age, String illness){
+        super(id,name,age);
+        this.illness = illness;
+    }
+
 }
